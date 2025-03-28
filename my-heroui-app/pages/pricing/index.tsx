9efde8b898db49337860b3fc4ba87cd3373bd2be
Roberto Magallanes <robmag_1507@hotmail.com>
2025-03-27 22:30:48 -0600

    HeroUI Initial Setup

--- my-heroui-app/pages/pricing/index.tsx
+++ my-heroui-app/pages/pricing/index.tsx
@@ -1,6 +1,13 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+
+
+
+
+
 export default function DocsPage() {
   return (
     <DefaultLayout>
@@ -8,6 +15,17 @@ export default function DocsPage() {
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>Pricing</h1>
         </div>
+
+        <div className="mt-8">
+          <Snippet hideCopyButton hideSymbol variant="bordered">
+            <span>
+              Get started by editing{" "}
+              <Code color="primary">pages/index.tsx</Code>
+            </span>
+          </Snippet>
+        </div>
+
+
       </section>
     </DefaultLayout>
   );
